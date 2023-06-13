5f4b55dd53cd8ea41cdcf96c30880b18553a3560
Jyoti-dholariya <121491970+Jyoti-dholariya@users.noreply.github.com>
2023-06-13 11:14:14 +0530

    changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const port = process.env || 8000;
+const port = process.env.PORT || 8000;
 require('./db/conn');
 const cors = require('cors');
 const users = require("./models/schema");
